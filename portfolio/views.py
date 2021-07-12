665ff01e2503b7d34acec5549798dd28e73bf0fb
rickjohnson2020 <eriksontokyo2020@gmail.com>
2021-07-12 14:50:43 +0900

    very first commit

---
+++ portfolio/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from .forms import ContactForm
+from .models import Profile, Work, Experience, Education, Skill, Hobby
+from django.conf import settings
+from django.core.mail import BadHeaderError, EmailMessage
+from django.http import HttpResponse
+import textwrap
+
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        profile_data = Profile.objects.all()
+        if profile_data.exists():
+            profile_data = profile_data.order_by('-id')[0]
+        work_data = Work.objects.order_by('-id')
+        return render(request, 'portfolio/index.html', {
+            'profile_data': profile_data,
+            'work_data': work_data
+        })
+
+class DetailView(View):
+    def get(self, request, *args, **kwargs):
+        work_data = Work.objects.get(id=self.kwargs['pk'])
+        return render(request, 'portfolio/detail.html', {
+            'work_data': work_data
+        })
+
+class AboutView(View):
+    def get(self, request, *args, **kwargs):
+        profile_data = Profile.objects.all()
+        if profile_data.exists():
+            profile_data = profile_data.order_by('-id')[0]
+        experience_data = Experience.objects.order_by('-id')
+        education_data = Education.objects.order_by('-id')
+        skill_data = Skill.objects.order_by('-id')
+        hobby_data = Hobby.objects.order_by('-id')
+        return render(request, 'portfolio/about.html', {
+            'profile_data': profile_data,
+            'experience_data': experience_data,
+            'education_data': education_data,
+            'skill_data': skill_data,
+            'hobby_data': hobby_data,
+        })
+
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        form = ContactForm(request.POST or None)
+        return render(request, 'portfolio/contact.html', {
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = ContactForm(request.POST or None)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            subject = 'お問い合わせありがとうございます。'
+            contact = textwrap.dedent('''
+                ※このメールはシステムからの自動返信です。
+
+                {name} 様
+
+                お問い合わせありがとうございました。
+                以下の内容でお問い合わせを受け付けいたしました。
+                内容を確認させていただき、ご返信させて頂きますので、少々お待ちください。
+
+                --------------------
+                ■お名前
+                {name}
+
+                ■メールアドレス
+                {email}
+
+                ■メッセージ
+                {message}
+                --------------------
+                ''').format(
+                    name=name,
+                    email=email,
+                    message=message
+                )
+            to_list = [email]
+            bcc_list = [settings.EMAIL_HOST_USER]
+
+            try:
+                message = EmailMessage(subject=subject, body=contact, to=to_list, bcc=bcc_list)
+                message.send()
+            except BadHeaderError:
+                return HttpResponse('無効なヘッダが検出されました。')
+
+            return redirect('thanks')
+
+        return render(request, 'portfolio/contact.html', {
+            'form': form
+        })
+
+
+class ThanksView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'portfolio/thanks.html')
